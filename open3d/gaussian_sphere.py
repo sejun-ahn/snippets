6991ac98184621cdb2d4b76d783fef1eadc8fca1
sejun-ahn <forstudytab7604@gmail.com>
2024-12-29 06:55:28 +0900

    Add image projection

--- open3d/gaussian_sphere.py
+++ open3d/gaussian_sphere.py
@@ -1,11 +1,22 @@
 # drawing a gaussian sphere
-__all__ = ['create_gaussian_sphere']
+__all__ = ['create_gaussian_sphere', 'create_x_plane', 'create_y_plane', 'create_z_plane', 'image_projection']
 
 import open3d as o3d
 import numpy as np
 
 def create_gaussian_sphere(resolution=20):
     sphere = o3d.geometry.TriangleMesh.create_sphere(radius=1.0, resolution=resolution)
+    r"""
+    Create a gaussian sphere.
+    Parameters
+    ----------
+    resolution : int, default = 20
+        Resolution of the sphere.
+    Returns
+    -------
+    open3d.geometry.TriangleMesh
+        A gaussian sphere.
+    """
     # https://www.open3d.org/docs/release/python_api/open3d.geometry.TriangleMesh.html#open3d.geometry.TriangleMesh.create_sphere
     # Being split by resolution,
     # a gaussian sphere will have
@@ -39,3 +50,88 @@ def create_gaussian_sphere(resolution=20):
     lineset.rotate(lineset.get_rotation_matrix_from_xyz((np.radians(90), 0, 0)), center=(0, 0, 0))
     
     return lineset
+
+def create_x_plane(thickness=0.01):
+    r"""
+    Create a torus representing the x-plane.
+    Parameters
+    ----------
+    thickness : float, default = 0.01
+        Thickness of the torus.
+    Returns
+    -------
+    open3d.geometry.TriangleMesh
+        A torus representing the x-plane.
+    """
+    x = o3d.geometry.TriangleMesh.create_torus(1.0, thickness)
+    x.paint_uniform_color([1, 0, 0])
+    x.rotate(x.get_rotation_matrix_from_xyz((0, np.radians(90), 0)), center=(0, 0, 0))
+    x.compute_vertex_normals()
+    return x
+
+def create_y_plane(thickness=0.01):
+    r"""
+    Create a torus representing the y-plane.
+    Parameters
+    ----------
+    thickness : float, default = 0.01
+        Thickness of the torus.
+    Returns
+    -------
+    open3d.geometry.TriangleMesh
+        A torus representing the y-plane.
+    """
+    y = o3d.geometry.TriangleMesh.create_torus(1.0, thickness)
+    y.paint_uniform_color([0, 1, 0])
+    y.rotate(y.get_rotation_matrix_from_xyz((np.radians(90), 0, 0)), center=(0, 0, 0))
+    y.compute_vertex_normals()
+    return y
+
+def create_z_plane(thickness=0.01):
+    r"""
+    Create a torus representing the z-plane.
+    Parameters
+    ----------
+    thickness : float, default = 0.01
+        Thickness of the torus.
+    Returns
+    -------
+    open3d.geometry.TriangleMesh
+        A torus representing the z-plane.
+    """
+    z = o3d.geometry.TriangleMesh.create_torus(1.0, thickness)
+    z.paint_uniform_color([0, 0, 1])
+    z.compute_vertex_normals()
+    return z
+
+def image_projection(image, fov_horizontal=60.0):
+    r"""
+    Project an image onto a sphere.
+    Parameters
+    ----------
+    image : numpy.ndarray
+        The image to be projected. The image should be in RGB format.
+    fov_horizontal : float, default = 60.0
+        Horizontal field of view in degrees.
+    Returns
+    -------
+    open3d.geometry.PointCloud
+        A point cloud representing the image pixels projected onto a sphere.
+    """
+    h,w,c = image.shape
+    fov_vertical = fov_horizontal * h / w
+    h_angles = np.linspace(-fov_horizontal/2, fov_horizontal/2, w)
+    v_angles = np.linspace(-fov_vertical/2, fov_vertical/2, h)
+    h_grid, v_grid = np.meshgrid(h_angles, v_angles)
+    h_grid = np.radians(h_grid)
+    v_grid = np.radians(v_grid)
+    x = np.cos(v_grid) * np.cos(h_grid)
+    y = np.cos(v_grid) * np.sin(h_grid)
+    z = np.sin(v_grid)
+    points = np.stack([x,y,z], axis=-1).reshape(-1,3)
+    colors = image.reshape(-1,3)/255.0
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points)
+    pcd.colors = o3d.utility.Vector3dVector(colors)
+    return pcd
+
--- open3d/gaussian_sphere_test.py
+++ open3d/gaussian_sphere_test.py
@@ -1,6 +1,7 @@
-from gaussian_sphere import create_gaussian_sphere
+from gaussian_sphere import *
 import open3d as o3d
 import numpy as np
+import cv2
 
 if __name__ == '__main__':
     vis = o3d.visualization.Visualizer()
@@ -8,6 +9,13 @@ if __name__ == '__main__':
     
     vis.add_geometry(o3d.geometry.TriangleMesh.create_coordinate_frame(size=1.0))
     vis.add_geometry(create_gaussian_sphere())
-    
+
+    vis.add_geometry(create_x_plane())
+    vis.add_geometry(create_y_plane())
+    vis.add_geometry(create_z_plane())
+
+    img = cv2.cvtColor(cv2.imread('milano.jpg'), cv2.COLOR_BGR2RGB)
+    vis.add_geometry(image_projection(img, 120.0))
+
     vis.run()
     vis.destroy_window()
